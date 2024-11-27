05aec3c273c99cd5e87f98efb5f0598c48c285ac
zhangli <2475934003@qq.com>
2024-11-27 19:40:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="pager">
+  <div class="text-header">
+        送给亲爱的帅鹏宝宝
+    </div>
+    <div class="container">
+      <div class="tree-layer">
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+      </div>
+      <div class="tree-layer">
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+      </div>
+      <div class="tree-layer">
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+      </div>
+      <div class="tree-layer">
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+      </div>
+      <div class="tree-layer">
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+      </div>
+      <div class="tree-layer">
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+        <div class="leaf"></div>
+      </div>
+      <div class="ornaments">
+        <div class="ornament-box">
+          <div class="string"></div>
+          <div class="ornament">
+            <div class="face">
+              <div class="eyes"></div>
+              <div class="shy-brows"></div>
+              <div class="shy-eyes"></div>
+              <div class="smile"></div>
+              <div class="tear"></div>
+            </div>
+          </div>
+        </div>
+        <div class="ornament-box">
+          <div class="string"></div>
+          <div class="ornament">
+            <div class="face">
+              <div class="eyes"></div>
+              <div class="sly-brows"></div>
+              <div class="lovey-dovey-eyes"></div>
+              <div class="blush"></div>
+              <div class="smile"></div>
+              <div class="kiss">
+                <div class="lip"></div>
+                <div class="lip"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="lights dip">
+        <div class="cord">
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+        </div>
+      </div>
+      <div class="lights-2 dip">
+        <div class="cord">
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+        </div>
+      </div>
+      <div class="lights-3">
+        <div class="cord">
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+          <div class="light"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
